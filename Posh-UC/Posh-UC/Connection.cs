d9a840282fd32742cb078e50e8b2a84798c204fa
jamessantiago <james.r.santiago@gmail.com>
2015-10-27 16:15:35 +0900

    initial

---
+++ Posh-UC/Posh-UC/Connection.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+using System.IO;
+using AxlNetClient;
+
+
+namespace Posh_UC
+{
+
+    public sealed class CurrentAxlClient
+    {
+        private static volatile CurrentAxlClient instance;
+        private static object syncRoot = new object();
+
+        private CurrentAxlClient()
+        {
+            Loaded = false;
+        }
+
+        public static CurrentAxlClient Instance
+        {
+            get
+            {
+                if (instance != null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new CurrentAxlClient();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public bool Loaded { get; private set; }
+        public AxlClient Client { get; private set; }
+
+        public void Connect(string Server, string Username, string Password)
+        {
+            Loaded = false;
+            var tempClient = new AxlClient(new AxlClientSettings
+            {
+                Server = Server,
+                User = Username,
+                Password = Password
+            });
+            var testResults = tempClient.Execute(client =>
+            {
+                var res = client.getUser(new GetUserReq
+                {
+                    ItemElementName = ItemChoiceType100.userid,
+                    Item = Username
+                });
+            });
+            if (testResults.Exception != null)
+                throw testResults.Exception;
+            else
+                Loaded = true;
+        }
+
+        public void Disconnect()
+        {
+            Client = null;
+            Loaded = false;
+        }
+
+    }
+
+    
+
+    [Cmdlet(VerbsCommunications.Connect, "UcServer")]
+    public class ConnectUcServer : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            CurrentAxlClient.Instance.Connect(Server, Username, Password);
+            if (CurrentAxlClient.Instance.Loaded)
+                WriteObject("Successfully connected the AXL client");
+        }
+
+        [Parameter(
+            ParameterSetName = "String",
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            ValueFromPipeline = true,
+            Position = 0,
+            HelpMessage = "AXL UC server to run the sql command against")]
+        public string Server;
+
+        [Parameter(
+            ParameterSetName = "String",
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            ValueFromPipeline = true,
+            Position = 1,
+            HelpMessage = "AXL username to connect with")]
+        public string Username;
+
+        [Parameter(
+            ParameterSetName = "String",
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            ValueFromPipeline = true,
+            Position = 2,
+            HelpMessage = "AXL password to connect with")]
+        public string Password;
+    }
+
+    [Cmdlet(VerbsCommunications.Disconnect, "UcServer")]
+    public class DisconnectUcServer : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            CurrentAxlClient.Instance.Disconnect();
+            WriteObject("Successfully disconnected the AXL client");
+        }
+    }
+
+    public class ClientNotLoadedException : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Client has not been loaded.  Use Connect-UcServer before executing any Posh-UC commands";
+            }
+        }
+    }
+}
---
+++ Posh-UC/Posh-UC/SQL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+using System.IO;
+using AxlNetClient;
+
+
+namespace Posh_UC
+{
+    [Cmdlet(VerbsLifecycle.Invoke, "UcSqlCommand")]
+    public class UcSqlCommand : PSCmdlet
+    {
+        protected override void BeginProcessing()
+        {
+            if (!CurrentAxlClient.Instance.Loaded)
+            {
+                throw new ClientNotLoadedException(); 
+            }
+        }
+
+        protected override void ProcessRecord()
+        {
+            var data = CurrentAxlClient.Instance.Client.Execute(client =>
+            {
+                var res = client.executeSQLQuery(new ExecuteSQLQueryReq
+                {
+                    sql = Command
+                });
+                return res.@return;
+            });
+            if (data.Exception != null)
+                throw data.Exception;
+            else
+                WriteObject(data.Value);
+        }
+
+        [Parameter(
+            ParameterSetName = "String",
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            ValueFromPipeline = true,
+            Position = 0,
+            HelpMessage = "Informix SQL command to run against the UC server")]
+        public string Command;
+    }
+}
